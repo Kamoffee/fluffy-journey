63f2694cf5ccf85cb38c1e1b3b2630e5b1f748d5
Andrei Miron <victormiron.home@gmail.com>
2024-01-27 18:29:11 -0500

    THis are new files

---
+++ negativeslices.py
@@ -0,0 +1,10 @@
+#         012345678901234
+parrot = "Norwegian Blue"
+
+print(parrot[0:6])   #Norweg
+print(parrot[-14:-8])
+print(parrot[-14:-5])
+print(parrot[:-5])
+
+print(parrot[-4:-2])   #Bl
+print(parrot[-4:12])   #Bl
